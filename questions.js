363d759e66a88c7abed8689b1922f9a58ce25d68
nikki0radz <129073884+nikki0radz@users.noreply.github.com>
2026-09-09 23:43:31 +0100

    Load persistent question decks and expanded banks

--- questions.js
+++ questions.js
@@ -1,12 +1,14 @@
 (()=>{
-  const v='20260909-axo5';
+  const v='20260909-history1';
   document.write(
     '<script src="questions-core.js?v='+v+'"><\/script>'+ 
     '<script src="enhancements.js?v='+v+'"><\/script>'+ 
     '<script src="trivia-extra.js?v='+v+'"><\/script>'+ 
+    '<script src="question-bank-extra.js?v='+v+'"><\/script>'+ 
     '<script src="settings-extra.js?v='+v+'"><\/script>'+ 
     '<script src="palette-extra.js?v='+v+'"><\/script>'+ 
     '<script src="seaweed-extra.js?v='+v+'"><\/script>'+ 
-    '<script src="axolotl-extra.js?v='+v+'"><\/script>'
+    '<script src="axolotl-extra.js?v='+v+'"><\/script>'+ 
+    '<script src="history-extra.js?v='+v+'"><\/script>'
   );
 })();
