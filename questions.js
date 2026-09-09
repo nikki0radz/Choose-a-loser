8ca23ce977a36928f90ffa80087a0d753d126ad8
nikki0radz <129073884+nikki0radz@users.noreply.github.com>
2026-09-09 11:28:29 +0100

    Load extra trivia and settings modules

--- questions.js
+++ questions.js
@@ -1,1 +1,1 @@
-document.write('<script src="questions-core.js"><\/script><script src="enhancements.js"><\/script>');
+document.write('<script src="questions-core.js"><\/script><script src="enhancements.js"><\/script><script src="trivia-extra.js"><\/script><script src="settings-extra.js"><\/script>');
