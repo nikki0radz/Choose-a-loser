3babd80372954ce5fbdaa0ea72f481396bfb73af
nikki0radz <129073884+nikki0radz@users.noreply.github.com>
2026-09-09 12:55:04 +0100

    Load new axolotl expressions and refresh assets

--- questions.js
+++ questions.js
@@ -1,11 +1,12 @@
 (()=>{
-  const v='20260909-1208';
+  const v='20260909-axo2';
   document.write(
     '<script src="questions-core.js?v='+v+'"><\/script>'+ 
     '<script src="enhancements.js?v='+v+'"><\/script>'+ 
     '<script src="trivia-extra.js?v='+v+'"><\/script>'+ 
     '<script src="settings-extra.js?v='+v+'"><\/script>'+ 
     '<script src="palette-extra.js?v='+v+'"><\/script>'+ 
-    '<script src="seaweed-extra.js?v='+v+'"><\/script>'
+    '<script src="seaweed-extra.js?v='+v+'"><\/script>'+ 
+    '<script src="axolotl-extra.js?v='+v+'"><\/script>'
   );
 })();
