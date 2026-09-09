eccec5296cee85e94ec444995a46ba888290e820
nikki0radz <129073884+nikki0radz@users.noreply.github.com>
2026-09-09 12:42:42 +0100

    Cache-bust app modules so updates appear reliably

--- questions.js
+++ questions.js
@@ -1,1 +1,11 @@
-document.write('<script src="questions-core.js"><\/script><script src="enhancements.js"><\/script><script src="trivia-extra.js"><\/script><script src="settings-extra.js"><\/script><script src="palette-extra.js"><\/script><script src="seaweed-extra.js"><\/script>');
+(()=>{
+  const v='20260909-1208';
+  document.write(
+    '<script src="questions-core.js?v='+v+'"><\/script>'+ 
+    '<script src="enhancements.js?v='+v+'"><\/script>'+ 
+    '<script src="trivia-extra.js?v='+v+'"><\/script>'+ 
+    '<script src="settings-extra.js?v='+v+'"><\/script>'+ 
+    '<script src="palette-extra.js?v='+v+'"><\/script>'+ 
+    '<script src="seaweed-extra.js?v='+v+'"><\/script>'
+  );
+})();
